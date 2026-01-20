62f822d70ae4df7dc30c57b8a5196454ef2230bd
Primuu <trentowski.adam@gmail.com>
2026-01-20 14:25:07 +0100

    Initial commit

---
+++ src/main/java/com/primuu/github_task/GithubtaskApplication.java
@@ -0,0 +1,13 @@
+package com.primuu.github_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primuu/github_task/GithubtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primuu.github_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
