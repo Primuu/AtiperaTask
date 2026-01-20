ee093c3563896a66265778ab8199dd77e9f85dc2
Primuu <trentowski.adam@gmail.com>
2026-01-20 17:15:02 +0100

    Refactor: separation of client and controller logic

--- src/main/java/com/primuu/github_task/Client.java
+++ src/main/java/com/primuu/github_task/Client.java
@@ -1,30 +1,31 @@
 package com.primuu.github_task;
 
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
 import java.util.List;
 
-@RestController
-public class Controller {
+@Component
+final class Client {
 
     private final RestClient restClient;
+
     private static final ParameterizedTypeReference<List<GithubRepoModel>> REPOS =
             new ParameterizedTypeReference<>() {};
 
-    public Controller(RestClient restClient) {
+    Client(RestClient restClient) {
         this.restClient = restClient;
     }
 
-    @GetMapping("/get-repos/{username}")
-    public List<GithubRepoModel> getUserReposByUsername(@PathVariable String username) {
+    List<GithubRepoModel> getUserReposByUsername(String username) {
         return restClient.get()
                 .uri("/users/{username}/repos", username)
                 .retrieve()
                 .body(REPOS);
+        // TODO: error handling
     }
 
 }
